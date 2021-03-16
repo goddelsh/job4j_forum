ee6d3b6458121f22b315b00ae89e5064d25a8047
Goddelsh <Goddelsh@icloud.com>
2021-03-16 14:47:16 +0500

    0. Spring Liquibase [#372058]

--- src/main/resources/scripts/inserts.sql
+++ src/main/resources/scripts/inserts.sql
@@ -1,14 +1,13 @@
-INSERT INTO public.athorities(
+INSERT INTO athorities(
 	id, role)
 	VALUES (1, 'ROLE_USER');
 
-	INSERT INTO public.athorities(
+	INSERT INTO athorities(
 	id, role)
 	VALUES (2, 'ROLE_ADMIN');
 
 
-	INSERT INTO public.users(
+	INSERT INTO users(
 	id, email, enabled, name, password, authority_id)
 	VALUES (1, 'root@mail.com', true, 'root', '$2a$10$aWlZ726biY80v.HlZ5INiuOwhOQSsWvBDY2Cz5l4RHNkymy0UiQEC', 2);
-
-    --пароль 123
+	    --пароль 123
